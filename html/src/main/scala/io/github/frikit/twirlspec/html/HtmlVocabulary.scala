45de95966a2ca6b5c35c8c533f0cf47add9c3dda
Victor Osipov <osipovvictor1994@gmail.com>
2026-09-13 09:42:59 +0100

    Carry one licence header in every source file, owned by frikiT
    Every Scala source and the test configuration now carry the same
    Apache 2.0 header, with frikiT as the copyright owner. The test
    suites and the coverage build helper had no header, the rest named
    Victor Osipov, and some had no blank line after the comment.

--- html/src/main/scala/io/github/frikit/twirlspec/html/HtmlVocabulary.scala
+++ html/src/main/scala/io/github/frikit/twirlspec/html/HtmlVocabulary.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2026 Victor Osipov
+ * Copyright 2026 frikiT
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.github.frikit.twirlspec.html
 
 /** The parts of the HTML vocabulary a checker needs to know to tell a mistake from a shorthand. */
--- html/src/main/scala/io/github/frikit/twirlspec/html/TwirlSource.scala
+++ html/src/main/scala/io/github/frikit/twirlspec/html/TwirlSource.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2026 Victor Osipov
+ * Copyright 2026 frikiT
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.github.frikit.twirlspec.html
 
 /** A Twirl template with the Scala taken out, so that what is left is the HTML it writes.
